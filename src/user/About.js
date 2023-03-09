e65b5030c0577d2ae3f808d85fd33b18862910ec
3r3bu5x9 <dbuxy@protonmail.com>
2023-03-09 21:04:11 +0530

    v1.7 added background

--- src/user/About.js
+++ src/user/About.js
@@ -1,19 +1,21 @@
 export default function About() {
     return (
-        <div className={'AboutContainer'}>
-            <h1>About</h1>
-            <br/>
-            <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h3>
-            <h5>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</h5>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum augue a rutrum auctor. Phasellus faucibus metus id sem mollis finibus. Nulla pretium scelerisque faucibus. Curabitur quis augue non mi viverra maximus a id ex. Aenean iaculis et ante vel tempus. Nunc feugiat dictum libero, nec facilisis tortor congue vulputate. Duis nec ultrices diam.
-            </p>
-            <p>
-                Aliquam sollicitudin neque at eleifend pellentesque. Curabitur mattis lorem at turpis tempus imperdiet. Aliquam erat volutpat. Maecenas porttitor lacus commodo posuere placerat. Cras efficitur lorem lorem, vitae placerat nulla commodo et. Sed sit amet felis at metus efficitur dapibus. Morbi nec viverra ante. Nullam consequat lectus purus, non mattis diam maximus et. Pellentesque tempor arcu nec molestie efficitur. Vivamus a dui risus. Nunc ac erat molestie, lobortis metus eget, cursus neque. Duis sagittis vel arcu et rutrum. Quisque commodo ipsum id egestas blandit. Sed eget porta leo. Ut nec efficitur arcu.
-            </p>
-            <p>
-                Maecenas tincidunt purus nec dapibus auctor. Donec et euismod diam. Nam aliquam aliquet dolor vel tincidunt. Sed efficitur auctor risus sed interdum. Sed venenatis a dolor sed ultrices. Duis imperdiet dolor elit, porttitor rhoncus quam convallis non. Aenean quis mauris sed eros ornare dapibus quis non neque. Integer quis urna nec ipsum posuere laoreet non quis lacus. Integer quis neque eleifend, pretium lectus maximus, volutpat elit. Aliquam ullamcorper justo at tellus convallis, vel bibendum sapien efficitur. Aenean id neque pharetra, pharetra diam mattis, vestibulum nibh. Vestibulum a est tristique, sollicitudin est sit amet, iaculis purus. Nulla posuere neque eget ante sodales, a interdum quam dictum. Nulla condimentum rutrum mauris ut finibus.
-            </p>
-        </div>
+        <center>
+            <div className={'AboutContainer'}>
+                <h1>About</h1>
+                <br/>
+                <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h3>
+                <h5>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</h5>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum augue a rutrum auctor. Phasellus faucibus metus id sem mollis finibus. Nulla pretium scelerisque faucibus. Curabitur quis augue non mi viverra maximus a id ex. Aenean iaculis et ante vel tempus. Nunc feugiat dictum libero, nec facilisis tortor congue vulputate. Duis nec ultrices diam.
+                </p>
+                <p>
+                    Aliquam sollicitudin neque at eleifend pellentesque. Curabitur mattis lorem at turpis tempus imperdiet. Aliquam erat volutpat. Maecenas porttitor lacus commodo posuere placerat. Cras efficitur lorem lorem, vitae placerat nulla commodo et. Sed sit amet felis at metus efficitur dapibus. Morbi nec viverra ante. Nullam consequat lectus purus, non mattis diam maximus et. Pellentesque tempor arcu nec molestie efficitur. Vivamus a dui risus. Nunc ac erat molestie, lobortis metus eget, cursus neque. Duis sagittis vel arcu et rutrum. Quisque commodo ipsum id egestas blandit. Sed eget porta leo. Ut nec efficitur arcu.
+                </p>
+                <p>
+                    Maecenas tincidunt purus nec dapibus auctor. Donec et euismod diam. Nam aliquam aliquet dolor vel tincidunt. Sed efficitur auctor risus sed interdum. Sed venenatis a dolor sed ultrices. Duis imperdiet dolor elit, porttitor rhoncus quam convallis non. Aenean quis mauris sed eros ornare dapibus quis non neque. Integer quis urna nec ipsum posuere laoreet non quis lacus. Integer quis neque eleifend, pretium lectus maximus, volutpat elit. Aliquam ullamcorper justo at tellus convallis, vel bibendum sapien efficitur. Aenean id neque pharetra, pharetra diam mattis, vestibulum nibh. Vestibulum a est tristique, sollicitudin est sit amet, iaculis purus. Nulla posuere neque eget ante sodales, a interdum quam dictum. Nulla condimentum rutrum mauris ut finibus.
+                </p>
+            </div>
+        </center>
     )
 }
